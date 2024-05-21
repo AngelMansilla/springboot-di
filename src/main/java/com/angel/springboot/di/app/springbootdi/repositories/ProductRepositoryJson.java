f4b3bf7674a02d18a9cf0b9a3228ecf0b129e710
Angel Mansilla Puerto <angelmansillapuerto@outlook.com>
2024-05-21 13:15:36 +0200

    Leer datosd e un archivo json

---
+++ src/main/java/com/angel/springboot/di/app/springbootdi/repositories/ProductRepositoryJson.java
@@ -0,0 +1,44 @@
+package com.angel.springboot.di.app.springbootdi.repositories;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.angel.springboot.di.app.springbootdi.models.Product;
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ProductRepositoryJson implements ProductRepository {
+
+    private List<Product> list;
+
+
+    public ProductRepositoryJson() {
+        Resource resource = new ClassPathResource(("json/product.json"));
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            list = Arrays.asList(objectMapper.readValue(resource.getFile(), Product[].class));
+        } catch (StreamReadException e) {
+            e.printStackTrace();
+        } catch (DatabindException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return list;
+    }
+
+    @Override
+    public Product findById(long id) {
+        return null;
+    }
+
+}
